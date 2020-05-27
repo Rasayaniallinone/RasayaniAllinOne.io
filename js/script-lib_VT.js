59bfc5bfb8247900f8fb323c9a973cd6aa3fccfe
Vikrant <vikrantthakur15@gmail.com>
2020-05-27 21:02:10 +0530

    Replace Marque tag

--- js/script-lib_VT.js
+++ js/script-lib_VT.js
@@ -667,7 +667,7 @@ function getOfferProduct(){
 		  url: context + "productOfferSection",
 		  data : JSON.stringify(map),
 		  success: function (response) { 
-					$("#offerinfo").html(response);
+					$(".offerMarqueNote").html("<p>"+response+'</p>');
 				},
 		  error : function (response) { 
 					
